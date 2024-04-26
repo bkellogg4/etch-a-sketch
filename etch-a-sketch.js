0f927dc09a3d11b5dcbb2a4aea6736dfd5f2e4d3
bkellogg4 <bkellogg4@gmail.com>
2024-04-26 14:26:42 -0500

    create 16x16 grid using 16 for loops

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,25 +1,92 @@
 function initializegame() {
-    let container = document.querySelector("#container");
-    let div1 = document.createElement('div');
-    div1.style.width = "100px";
-    div1.style.height = "100px";
-    div1.style.border = "5px solid black";
-    div1.style.display = "flex";
-    container.appendChild(div1);
+    
+    for (let i = 0; i <= 16; i++) {
+        let row1 = document.querySelector("#row-one");
+        let div = document.createElement('div');
+        div.setAttribute("style", "width: 100px; height: 100px; border: 5px solid black;");
+        row1.appendChild(div);
+    };
 
-    let div2 = document.createElement('div');
-    div2.style.width = "100px";
-    div2.style.height = "100px";
-    div2.style.border = "5px solid red";
-    div2.style.display = "flex";
-    container.appendChild(div2);
+    for (let i = 0; i <= 16; i++) {    
+        let row2 = document.querySelector("#row-two");
+        let div = document.createElement('div');
+        div.setAttribute("style", "width: 100px; height: 100px; border: 5px solid black;");
+        row2.appendChild(div);
 
-    let div3 = document.createElement('div');
-    div3.style.width = "100px";
-    div3.style.height = "100px";
-    div3.style.border = "5px solid orange";
-    div3.style.display = "flex";
-    container.appendChild(div3);
-}
+    };
+
+    for (let i = 0; i <= 16; i++) {    
+        let row3 = document.querySelector("#row-three");
+        let div = document.createElement('div');
+        div.setAttribute("style", "width: 100px; height: 100px; border: 5px solid black;");
+        row3.appendChild(div);
+    };
+
+    for (let i = 0; i <= 16; i++) {    
+        let row4 = document.querySelector("#row-four");
+        let div = document.createElement('div');
+        div.setAttribute("style", "width: 100px; height: 100px; border: 5px solid black;");
+        row4.appendChild(div);
+    };     
+    
+    for (let i = 0; i <= 16; i++) {    
+        let row5 = document.querySelector("#row-five");
+        let div = document.createElement('div');
+        div.setAttribute("style", "width: 100px; height: 100px; border: 5px solid black;");
+        row5.appendChild(div);
+    };
+
+    for (let i = 0; i <= 16; i++) {    
+        let row6 = document.querySelector("#row-six");
+        let div = document.createElement('div');
+        div.setAttribute("style", "width: 100px; height: 100px; border: 5px solid black;");
+        row6.appendChild(div);
+    };
+
+    for (let i = 0; i <= 16; i++) {
+        let row7 = document.querySelector("#row-seven");
+        let div = document.createElement('div');
+        div.setAttribute("style", "width: 100px; height: 100px; border: 5px solid black;");
+        row7.appendChild(div);
+    };
+
+    for (let i = 0; i <= 16; i++) {    
+        let row8 = document.querySelector("#row-eight");
+        let div = document.createElement('div');
+        div.setAttribute("style", "width: 100px; height: 100px; border: 5px solid black;");
+        row8.appendChild(div);
+
+    };
+
+    for (let i = 0; i <= 16; i++) {    
+        let row9 = document.querySelector("#row-nine");
+        let div = document.createElement('div');
+        div.setAttribute("style", "width: 100px; height: 100px; border: 5px solid black;");
+        row9.appendChild(div);
+    };
+
+    for (let i = 0; i <= 16; i++) {    
+        let row10 = document.querySelector("#row-ten");
+        let div = document.createElement('div');
+        div.setAttribute("style", "width: 100px; height: 100px; border: 5px solid black;");
+        row10.appendChild(div);
+    };     
+    
+    for (let i = 0; i <= 16; i++) {    
+        let row11 = document.querySelector("#row-eleven");
+        let div = document.createElement('div');
+        div.setAttribute("style", "width: 100px; height: 100px; border: 5px solid black;");
+        row11.appendChild(div);
+    };
+
+    for (let i = 0; i <= 16; i++) {    
+        let row12 = document.querySelector("#row-twelve");
+        let div = document.createElement('div');
+        div.setAttribute("style", "width: 100px; height: 100px; border: 5px solid black;");
+        row12.appendChild(div);
+    };
+
+    
+};
 
 initializegame();
